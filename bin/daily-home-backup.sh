ce8688150dd06fc72c9f21628b1bed2c6f11a20b
Pawel Kruszewski <kruszewsky@gmail.com>
2019-08-24 18:59:07 +0200

    backup system setup configuration along with home directory daily

--- bin/daily-home-backup.sh
+++ bin/daily-home-backup.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 rsync -av --delete --delete-excluded --exclude-from "$HOME/.local/home-backup/rsync.exclude.files.txt" $HOME "/run/media/ktor/home-backup/daily"
+rsync -av --delete --delete-excluded /etc/nixos "/run/media/ktor/home-backup/nixos"
