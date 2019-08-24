5c065b1a0046695cc53c58a792bab25d74d80ebb
Pawel Kruszewski <kruszewsky@gmail.com>
2019-08-24 18:59:39 +0200

    latest monitor config at work

--- bin/monitor-aupark.sh
+++ bin/monitor-aupark.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 xrandr \
-  --output HDMI-2 --auto \
-  --output DP-1 --auto --right-of HDMI-2 --primary \
+  # --output DP-1 --auto  --primary \
+  # --output eDP-1 --auto --right-of DP-1
+xrandr \
+  --output HDMI-1 --auto \
+  --output DP-1 --auto --right-of HDMI-1 --primary \
   --output eDP-1 --auto --right-of DP-1
 restart-apps.sh
