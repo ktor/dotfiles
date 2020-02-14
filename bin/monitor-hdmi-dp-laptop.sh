4d6f45e3d17fb7e44ebb8eec440b2c5705f62bd5
Pawel Kruszewski <kruszewsky@gmail.com>
2020-02-14 15:55:56 +0100

    adjust monitor scripts

--- bin/monitor-hdmi-dp-laptop.sh
+++ bin/monitor-hdmi-dp-laptop.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 xrandr \
-  # --output DP-1 --auto  --primary \
-  # --output eDP-1 --auto --right-of DP-1
-xrandr \
   --output HDMI-1 --auto \
   --output DP-1 --auto --right-of HDMI-1 --primary \
   --output eDP-1 --auto --right-of DP-1
--- bin/monitor-home.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-xrandr --auto --output eDP-1 --auto --output HDMI-2 --primary --right-of eDP-1
-restart-apps.sh
