b13898e362976bae17132cfa8ca24fe099aba0a6
Pawel Kruszewski <kruszewsky@gmail.com>
2021-03-06 11:22:42 +0100

    open MS mime types with libreoffice

--- bin/mime-type-setup.sh
+++ bin/mime-type-setup.sh
@@ -4,3 +4,7 @@ xdg-mime default brave-browser.desktop text/html
 xdg-mime default teams.desktop x-scheme-handler/msteams
 xdg-mime default teams.desktop x-scheme-handler/teams
 xdg-mime default thunderbird.desktop x-scheme-handler/mailto
+xdg-mime default writer.desktop application/vnd.openxmlformats-officedocument.wordprocessingml.document
+xdg-mime default calc.desktop application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+xdg-mime default impress.desktop application/vnd.openxmlformats-officedocument.presentationml.presentation
+
