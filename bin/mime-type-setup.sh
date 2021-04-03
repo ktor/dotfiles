fdb78e2c4e669c26c14df4f183748a4684bdc078
Pawel Kruszewski <kruszewsky@gmail.com>
2021-04-03 20:52:49 +0200

    remove obsolete code

--- bin/mime-type-setup.sh
+++ bin/mime-type-setup.sh
@@ -1,5 +1,4 @@
 xdg-settings set default-web-browser brave-browser.desktop
-xdg-settings set default-web-browser brave-browser.desktop
 xdg-mime default brave-browser.desktop text/html
 xdg-mime default teams.desktop x-scheme-handler/msteams
 xdg-mime default teams.desktop x-scheme-handler/teams
