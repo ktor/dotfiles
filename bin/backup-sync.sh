c82a95be5674501527fc838f2c55abf53b27bd21
Pawel Kruszewski <kruszewsky@gmail.com>
2021-06-16 14:20:12 +0200

    fix backup sync

--- bin/backup-sync.sh
+++ bin/backup-sync.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -av --delete "/run/media/ktor/DISK 1/double_copy" "/run/media/ktor/DISK 2/double_copy"
+rsync -av --delete "/run/media/ktor/DISK 1/double_copy" "/run/media/ktor/DISK 2"
